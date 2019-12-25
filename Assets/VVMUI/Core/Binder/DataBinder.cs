8a5569b1b4e2cfc348269a0e6856084999e75887
xiaohuai <279727697@qq.com>
2019-12-25 20:59:30 +0800

    实现可交互组件的数据双向绑定

--- Assets/VVMUI/Core/Binder/DataBinder.cs
+++ Assets/VVMUI/Core/Binder/DataBinder.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 using VVMUI.Core.Converter;
 using VVMUI.Core.Data;
 
@@ -10,8 +12,29 @@ namespace VVMUI.Core.Binder {
     public class DataBinder : BaseDataBinder {
         [Serializable]
         public class DataBinderItem {
+            public enum BindType {
+                Property,
+                Active,
+                Animation,
+                Animator
+            }
+
+            public BindType Type;
+
+            // property bind vars
             public Component Component;
             public string Property;
+
+            // active bind vars
+            public GameObject GameObject;
+
+            // animation bind vars
+            public Animation Animation;
+
+            // animator bind vars
+            public Animator Animator;
+            public int AnimatorLayer;
+
             public DataDefiner Definer = new DataDefiner ();
 
             [HideInInspector]
@@ -23,7 +46,55 @@ namespace VVMUI.Core.Binder {
             [HideInInspector]
             public Action SetValueHandler;
 
+            [HideInInspector]
+            public object ValueChangedHandler;
+
+            private bool CheckDataTypeValid (Type t, GameObject obj) {
+                if (this.Source == null) {
+                    Debugger.LogError ("DataBinder", obj.name + " data null.");
+                    return false;
+                }
+
+                Type propertyType = t;
+                Type dataType = this.Source.GetType ();
+                Type dataBaseType = dataType.BaseType;
+
+                bool bindData = false;
+                if (this.Converter != null) {
+                    bindData = true;
+                }
+
+                // TODO 性能优化：Type.GetGenericArguments
+                if (dataBaseType.IsGenericType && dataBaseType.GetGenericArguments () [0] == propertyType) {
+                    bindData = true;
+                }
+
+                if (!bindData) {
+                    Debugger.LogError ("DataBinder", obj.name + " data type not explicit or converter is null.");
+                    return false;
+                }
+
+                return true;
+            }
+
             public void DoBind (VMBehaviour vm, GameObject obj) {
+                switch (this.Type) {
+                    case BindType.Property:
+                        DoPropertyBind (vm, obj);
+                        break;
+                    case BindType.Active:
+                        DoActiveBind (vm, obj);
+                        break;
+                    case BindType.Animation:
+                        DoAnimationBind (vm, obj);
+                        break;
+                    case BindType.Animator:
+                        DoAnimatorBind (vm, obj);
+                        break;
+                }
+            }
+
+            private void DoPropertyBind (VMBehaviour vm, GameObject obj) {
                 if (this.Component == null) {
                     Debugger.LogError ("DataBinder", obj.name + " component null.");
                     return;
@@ -41,31 +112,13 @@ namespace VVMUI.Core.Binder {
                     return;
                 }
 
-                if (this.Source == null) {
-                    Debugger.LogError ("DataBinder", obj.name + " data null.");
-                    return;
-                }
-
                 Type propertyType = propertyInfo.PropertyType;
-                Type dataType = this.Source.GetType ();
-                Type dataBaseType = dataType.BaseType;
-
-                bool bindData = false;
-                if (this.Converter != null) {
-                    bindData = true;
-                }
-
-                // TODO 性能优化：Type.GetGenericArguments
-                if (dataBaseType.IsGenericType && dataBaseType.GetGenericArguments () [0] == propertyType) {
-                    bindData = true;
-                }
-
-                if (!bindData) {
-                    Debugger.LogError ("DataBinder", obj.name + " data type not explicit or converter is null.");
+                if (!CheckDataTypeValid (propertyType, obj)) {
                     return;
                 }
 
                 // TODO 性能优化：Type.GetMethod
+                Type dataType = this.Source.GetType ();
                 MethodInfo getMethod = dataType.GetMethod ("Get");
                 this.SetValueHandler = delegate () {
                     // TODO 性能优化：MethodInfo.Invoke
@@ -78,9 +131,164 @@ namespace VVMUI.Core.Binder {
                 };
                 this.Source.ValueChanged += this.SetValueHandler;
                 this.SetValueHandler.Invoke ();
+
+                // 可交互组件的双向绑定
+                if (componentType == typeof (Toggle) && this.Property.Equals ("isOn")) {
+                    this.ValueChangedHandler = new UnityAction<bool> (delegate (bool arg) {
+                        (this.Source as BoolData).Set (arg);
+                    });
+                    (this.Component as Toggle).onValueChanged.AddListener ((UnityAction<bool>) this.ValueChangedHandler);
+                }
+                if (componentType == typeof (InputField) && this.Property.Equals ("text")) {
+                    this.ValueChangedHandler = new UnityAction<string> (delegate (string arg) {
+                        (this.Source as StringData).Set (arg);
+                    });
+                    (this.Component as InputField).onValueChanged.AddListener ((UnityAction<string>) this.ValueChangedHandler);
+                }
+                if (componentType == typeof (Dropdown) && this.Property.Equals ("value")) {
+                    this.ValueChangedHandler = new UnityAction<int> (delegate (int arg) {
+                        (this.Source as IntData).Set (arg);
+                    });
+                    (this.Component as Dropdown).onValueChanged.AddListener ((UnityAction<int>) this.ValueChangedHandler);
+                }
+                if (componentType == typeof (Slider) && this.Property.Equals ("value")) {
+                    this.ValueChangedHandler = new UnityAction<float> (delegate (float arg) {
+                        (this.Source as FloatData).Set (arg);
+                    });
+                    (this.Component as Slider).onValueChanged.AddListener ((UnityAction<float>) this.ValueChangedHandler);
+                }
+            }
+
+            private void DoActiveBind (VMBehaviour vm, GameObject obj) {
+                if (this.GameObject == null) {
+                    Debugger.LogError ("DataBinder", obj.name + " gameobject null.");
+                    return;
+                }
+
+                Type propertyType = typeof (bool);
+                if (!CheckDataTypeValid (propertyType, obj)) {
+                    return;
+                }
+
+                // TODO 性能优化：Type.GetMethod
+                Type dataType = this.Source.GetType ();
+                MethodInfo getMethod = dataType.GetMethod ("Get");
+                this.SetValueHandler = delegate () {
+                    // TODO 性能优化：MethodInfo.Invoke
+                    object value = getMethod.Invoke (this.Source, null);
+                    if (this.Converter != null) {
+                        value = this.Converter.Convert (value, propertyType, this.Definer.ConverterParameter, vm);
+                    }
+                    this.GameObject.SetActive ((bool) value);
+                };
+                this.Source.ValueChanged += this.SetValueHandler;
+                this.SetValueHandler.Invoke ();
+            }
+
+            private void DoAnimationBind (VMBehaviour vm, GameObject obj) {
+                if (this.Animation == null) {
+                    Debugger.LogError ("DataBinder", obj.name + " Animation null.");
+                    return;
+                }
+
+                Type propertyType = typeof (string);
+                if (!CheckDataTypeValid (propertyType, obj)) {
+                    return;
+                }
+
+                // TODO 性能优化：Type.GetMethod
+                Type dataType = this.Source.GetType ();
+                MethodInfo getMethod = dataType.GetMethod ("Get");
+                this.SetValueHandler = delegate () {
+                    // TODO 性能优化：MethodInfo.Invoke
+                    object value = getMethod.Invoke (this.Source, null);
+                    if (this.Converter != null) {
+                        value = this.Converter.Convert (value, propertyType, this.Definer.ConverterParameter, vm);
+                    }
+                    this.Animation.Stop ();
+                    this.Animation.Play ((string) value, PlayMode.StopAll);
+                };
+                this.Source.ValueChanged += this.SetValueHandler;
+                this.SetValueHandler.Invoke ();
+            }
+
+            private void DoAnimatorBind (VMBehaviour vm, GameObject obj) {
+                if (this.Animator == null) {
+                    Debugger.LogError ("DataBinder", obj.name + " Animator null.");
+                    return;
+                }
+
+                Type propertyType = typeof (string);
+                if (!CheckDataTypeValid (propertyType, obj)) {
+                    return;
+                }
+
+                // TODO 性能优化：Type.GetMethod
+                Type dataType = this.Source.GetType ();
+                MethodInfo getMethod = dataType.GetMethod ("Get");
+                this.SetValueHandler = delegate () {
+                    // TODO 性能优化：MethodInfo.Invoke
+                    object value = getMethod.Invoke (this.Source, null);
+                    if (this.Converter != null) {
+                        value = this.Converter.Convert (value, propertyType, this.Definer.ConverterParameter, vm);
+                    }
+                    this.Animator.Play ((string) value, this.AnimatorLayer);
+                };
+                this.Source.ValueChanged += this.SetValueHandler;
+                this.SetValueHandler.Invoke ();
             }
 
             public void DoUnBind () {
+                switch (this.Type) {
+                    case BindType.Property:
+                        DoPropertyUnBind ();
+                        break;
+                    case BindType.Active:
+                        DoActiveUnBind ();
+                        break;
+                    case BindType.Animation:
+                        DoAnimationUnBind ();
+                        break;
+                    case BindType.Animator:
+                        DoAnimatorUnBind ();
+                        break;
+                }
+            }
+
+            private void DoPropertyUnBind () {
+                if (this.Source != null && this.SetValueHandler != null) {
+                    this.Source.ValueChanged -= this.SetValueHandler;
+                }
+                if (this.ValueChangedHandler != null) {
+                    Type componentType = this.Component.GetType ();
+                    if (componentType == typeof (Toggle) && this.Property.Equals ("isOn")) {
+                        (this.Component as Toggle).onValueChanged.RemoveListener ((UnityAction<bool>) this.ValueChangedHandler);
+                    }
+                    if (componentType == typeof (InputField) && this.Property.Equals ("text")) {
+                        (this.Component as InputField).onValueChanged.RemoveListener ((UnityAction<string>) this.ValueChangedHandler);
+                    }
+                    if (componentType == typeof (Dropdown) && this.Property.Equals ("value")) {
+                        (this.Component as Dropdown).onValueChanged.RemoveListener ((UnityAction<int>) this.ValueChangedHandler);
+                    }
+                    if (componentType == typeof (Slider) && this.Property.Equals ("value")) {
+                        (this.Component as Slider).onValueChanged.RemoveListener ((UnityAction<float>) this.ValueChangedHandler);
+                    }
+                }
+            }
+
+            private void DoActiveUnBind () {
+                if (this.Source != null && this.SetValueHandler != null) {
+                    this.Source.ValueChanged -= this.SetValueHandler;
+                }
+            }
+
+            private void DoAnimationUnBind () {
+                if (this.Source != null && this.SetValueHandler != null) {
+                    this.Source.ValueChanged -= this.SetValueHandler;
+                }
+            }
+
+            private void DoAnimatorUnBind () {
                 if (this.Source != null && this.SetValueHandler != null) {
                     this.Source.ValueChanged -= this.SetValueHandler;
                 }
