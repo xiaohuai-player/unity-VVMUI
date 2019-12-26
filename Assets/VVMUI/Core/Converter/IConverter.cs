39d8a039e2099f54d541028de9c12871cb3707e8
xiaohuai <279727697@qq.com>
2019-12-26 11:01:32 +0800

    转换器支持双向绑定转换

--- Assets/VVMUI/Core/Converter/IConverter.cs
+++ Assets/VVMUI/Core/Converter/IConverter.cs
@@ -3,5 +3,6 @@ using System;
 namespace VVMUI.Core.Converter {
     public interface IConverter {
         object Convert (object value, Type targetType, object parameter, VMBehaviour context);
+        object ConvertBack (object value, Type targetType, object parameter, VMBehaviour context);
     }
 }
--- Assets/VVMUI/Core/Converter/StringConverter.cs
+++ Assets/VVMUI/Core/Converter/StringConverter.cs
@@ -5,5 +5,9 @@ namespace VVMUI.Core.Converter {
         public object Convert (object target, Type targetType, object parameter, VMBehaviour context) {
             return System.Convert.ToString (target);
         }
+
+        public object ConvertBack (object target, Type targetType, object parameter, VMBehaviour context) {
+            return target;
+        }
     }
 }
