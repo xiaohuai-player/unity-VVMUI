2a1bc7dada55d1551d618b7e88cf6010371b62f7
xiaohuai <xiaohuai9455@gmail.com>
2020-01-21 13:47:32 +0800

    测试双向绑定

--- Assets/TestCase/TestComplexVM.cs
+++ Assets/TestCase/TestComplexVM.cs
@@ -9,6 +9,16 @@ using VVMUI.Core.Converter;
 using VVMUI.Core.Data;
 
 public class TestComplexVM : VMBehaviour {
+    public class IntToStringConverter : IConverter {
+        public object Convert (object value, Type targetType, object parameter, VMBehaviour context) {
+            return System.Convert.ToString (value);
+        }
+
+        public object ConvertBack (object value, Type targetType, object parameter, VMBehaviour context) {
+            return System.Convert.ToInt32 (value);
+        }
+    }
+
     public class FriendData : StructData {
         public StringData Name;
         public IntData Age;
@@ -28,24 +38,9 @@ public class TestComplexVM : VMBehaviour {
         }
     };
 
-    public StringData strInputName = "";
-    public IntData intInputAge = 0;
-
-    public StringCommand iptNameChanged;
-    public bool iptNameChanged_CanExecute (object parameter) {
-        return true;
-    }
-    public void iptNameChanged_Execute (string input, object parameter) {
-        strInputName.Set (input);
-    }
-
-    public StringCommand iptAgeChanged;
-    public bool iptAgeChanged_CanExecute (object parameter) {
-        return true;
-    }
-    public void iptAgeChanged_Execute (string input, object parameter) {
-        intInputAge.Set (Int32.Parse (input));
-    }
+    public StringData strInputName = "测试输入";
+    public IntData intInputAge = 99;
+    public IntToStringConverter cvtTest = new IntToStringConverter ();
 
     public ButtonCommand btnAddFriend;
     public bool btnAddFriend_CanExecute (object parameter) {
@@ -57,20 +52,20 @@ public class TestComplexVM : VMBehaviour {
                 Age = intInputAge.Get (),
                 IsOnline = true
         });
+        strInputName.Set ("");
+        intInputAge.Set (0);
     }
 
     public ButtonCommand btnDelFriend;
-    public bool btnDelFriend_CanExecute (object parameter) {
+    public bool btnDelFriend_CanExecute (object index) {
         return true;
     }
-    public void btnDelFriend_Execute (object parameter) {
-        friends.RemoveAt ((int) parameter);
+    public void btnDelFriend_Execute (object index) {
+        friends.RemoveAt ((int) index);
     }
 
     protected override void BeforeAwake () {
         btnAddFriend = new ButtonCommand (btnAddFriend_CanExecute, btnAddFriend_Execute);
         btnDelFriend = new ButtonCommand (btnDelFriend_CanExecute, btnDelFriend_Execute);
-        iptNameChanged = new StringCommand (iptNameChanged_CanExecute, iptNameChanged_Execute);
-        iptAgeChanged = new StringCommand (iptAgeChanged_CanExecute, iptAgeChanged_Execute);
     }
 }
